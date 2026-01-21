236bf47e6bc0937f98e3e91a8ec5b4fe00bf426c
Rafael Ito <ito.rafael@gmail.com>
2026-01-21 08:48:38 -0300

    [lan-mouse] add lanmouse-monitor script for desktops (server side)

---
+++ lan-mouse/lanmouse-monitor-laptop.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+# description:
+#   script to share clipboard via lan-mouse (software KVM) with the sender host.
+# how it works:
+#   it monitors the output of the journal for the lan-mouse unit, and once it
+#   detects that the cursor moved back to the sender, it runs push-clipboard.sh
+#   script that sends the clipboard back via SSH forced commands (previously
+#   configured in ~/.ssh/authorized-keys).
+
+# Usage:
+#   This is script is meant to be used as a daemon. A systemd user service
+#   is responsible for launching and controlling it.
+
+# define systemd unit to monitor
+UNIT="lan-mouse"
+# define message to monitor on the output of journalctl
+TRIGGER_ENTERING=".*releasing capture: [0-9.:]+ entered this device.*"
+TRIGGER_LEAVING="releasing capture: no active client at this position"
+# set cooldown (in seconds) to avoid double trigger
+COOLDOWN=1
+LAST_TRIGGER=0
+
+# monitor the journal for the specific user unit
+journalctl --user -u $UNIT -f -n 0 | while read -r line; do
+    CURRENT_TIME=$(date +%s)
+
+    # leaving this host --> returning to desktop
+    if [[ "$line" == *"$TRIGGER_LEAVING"* ]]; then
+        # check if enough time (cooldown) has passed since the last trigger
+        if ((CURRENT_TIME - LAST_TRIGGER >= COOLDOWN)); then
+            echo -n "[$(date '+%Y-%m-%d %H:%M:%S')] Mouse returned to PC. Sending clipboard..."
+            # send clipboard via SSH forced command
+            $HOME/.config/scripts/push-clipboard.sh catuaba >/dev/null
+            # append "Done!" or "Failed!" to the previous line according to the script status
+            [ $? -eq 0 ] && echo " Done!" || echo " Failed!"
+            LAST_TRIGGER=$CURRENT_TIME
+        fi
+
+        ## entering this host
+        #elif [[ "$line" =~ $TRIGGER_ENTERING ]]; then
+        #    #IP_ADDRESS=${BASH_REMATCH[1]}  # --> not working
+        #    echo -n "[$(date '+%Y-%m-%d %H:%M:%S')] Mouse just entered in this host. Do something useful..."
+        #    #ssh catuaba 'export SWAYSOCK=$(ls -t /run/user/1000/sway-ipc.1000.*.sock | head -1); swaymsg "[app_id=edge_bottom] focus"'
+        #    # append "Done!" or "Failed!" to the previous line according to the script status
+        #    [ $? -eq 0 ] && echo " Done!" || echo " Failed!"
+
+    fi
+done
